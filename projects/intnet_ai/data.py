e7b381815542bd5b0fe6db1fa18d78f0b690a27d
StarDust130 <csyadav0513@gmail.com>
2026-07-01 12:47:38 +0530

    Lets start

---
+++ projects/intnet_ai/data.py
@@ -0,0 +1,23 @@
+training_data = [
+
+    ("Where is my order?", "Track"),
+    ("Track my package", "Track"),
+    ("Where is my package?", "Track"),
+    ("My parcel hasn't arrived", "Track"),
+
+    ("I want a refund", "Refund"),
+    ("Return my money", "Refund"),
+    ("Give my money back", "Refund"),
+    ("Refund my payment", "Refund"),
+
+    ("Cancel my order", "Cancel"),
+    ("Delete my order", "Cancel"),
+    ("Stop my order", "Cancel"),
+    ("I don't want this order", "Cancel"),
+
+    ("I forgot my password", "Password"),
+    ("Reset my password", "Password"),
+    ("Can't login", "Password"),
+    ("Help me login", "Password")
+
+]
