1497d3009f907d161e45f1546c77e36ce561a762
StarDust130 <csyadav0513@gmail.com>
2026-07-01 21:25:35 +0530

    done all ✅

--- projects/intentbrain_ai/predict.py
+++ projects/intentbrain_ai/predict.py
@@ -5,13 +5,36 @@ from model import IntentBrain
 from data import training_data
 from utils import build_vocab
 
+
+
+from utils import (
+    sentence_to_ids,
+    label_map
+)
+
 vocab = build_vocab(training_data)
 
 model = IntentBrain(
     vocab_size=len(vocab),
     embedding_dim=16,
-    num_classes=4
+    num_classes=4,
+    
 )
 
 model.load_state_dict(torch.load("model.pth"))
 model.eval()
+
+while True:
+
+    sentence = input("You: ")
+
+    x = sentence_to_ids(sentence, vocab)
+
+    with torch.no_grad():
+        output = model(x)
+
+        prediction = int(torch.argmax(output).item())
+
+    labels = list(label_map.keys())
+
+    print("Intent:", labels[prediction])
