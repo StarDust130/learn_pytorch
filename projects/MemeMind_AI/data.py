3992fb4cbb752041ef7b2780b8f3e03169801e27
StarDust130 <csyadav0513@gmail.com>
2026-07-06 13:40:14 +0530

    l-1 done

---
+++ projects/MemeMind_AI/data.py
@@ -0,0 +1,15 @@
+training_data = [
+
+    ("Why did the chicken cross the road? To get to the other side.", "Funny"),
+
+    ("My code worked on the first try.", "Funny"),
+
+    ("Today is Monday.", "Dead"),
+
+    ("The sky is blue.", "Dead"),
+
+    ("Cats are animals.", "Normal"),
+
+    ("Water is wet.", "Normal"),
+
+]
---
+++ projects/MemeMind_AI/train.py
@@ -0,0 +1,6 @@
+from data import training_data
+
+print(training_data)
+print()
+
+print("Total Examples:", len(training_data))
