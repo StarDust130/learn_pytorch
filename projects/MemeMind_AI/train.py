c91a85a050cf67ec2d59cef2828e91e2fba75bc6
StarDust130 <csyadav0513@gmail.com>
2026-07-07 12:31:35 +0530

    data improve

--- projects/MemeMind_AI/train.py
+++ projects/MemeMind_AI/train.py
@@ -1,6 +1,8 @@
 from dataset import MemeDataset
 from torch.utils.data import DataLoader
 
+from model import MemeBrain
+
 dataset = MemeDataset()
 
 loader = DataLoader(
@@ -9,8 +11,11 @@ loader = DataLoader(
     shuffle=True
 )
 
-for batch in loader:
+model = MemeBrain()
 
-    print(batch)
+batch = next(iter(loader))
 
-    break
+output = model(batch["input_ids"])
+
+print(output)
+print(output.shape)
