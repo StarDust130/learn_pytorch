4c68b34c9ae5816c6e98677beebf3098b2fcb23e
StarDust130 <csyadav0513@gmail.com>
2026-07-11 14:46:27 +0530

    Og training

--- projects/SmartIntent_AI/train.py
+++ projects/SmartIntent_AI/train.py
@@ -1,23 +1,102 @@
-from torch.utils.data import DataLoader
-
+from model import SmartIntentAI
 from dataset import IntentDataset
 
+from torch.utils.data import DataLoader
+
+import torch
+import torch.nn as nn
+
+# 📦 Dataset
 dataset = IntentDataset()
 
 loader = DataLoader(
-
     dataset,
-
-    batch_size=32,
-
+    batch_size=16,
     shuffle=True
-
 )
 
-batch = next(iter(loader))
+# 🧠 Model
+model = SmartIntentAI()
 
-print(batch["input_ids"].shape)
+# 👨‍🏫 Teacher
+loss_fn = nn.CrossEntropyLoss()
 
-print(batch["attention_mask"].shape)
+# 🔧 Optimizer
+optimizer = torch.optim.AdamW(
+    model.parameters(),
+    lr=1e-4
+)
 
-print(batch["label"])
+# ===================================================
+# ===================================================
+# Traning Loop 🚂🚃🚃🚃
+epochs = 20
+
+for epoch in range(epochs):
+
+    total_loss = 0
+
+    for batch in loader:
+
+        input_ids = batch["input_ids"]
+
+        labels = batch["label"]
+
+        prediction = model(input_ids)
+
+        loss = loss_fn(
+            prediction,
+            labels
+        )
+
+        optimizer.zero_grad()
+
+        loss.backward()
+
+        optimizer.step()
+
+        total_loss += loss.item()
+
+    print(
+        f"Epoch {epoch+1} | Loss = {total_loss:.4f}"
+    )
+
+# ==========================================================
+# ==========================================================
+# 📊 Evaluation
+correct = 0
+total = 0
+
+model.eval()
+
+with torch.no_grad():
+
+    for batch in loader:
+
+        prediction = model(
+            batch["input_ids"]
+        )
+
+        predicted = prediction.argmax(dim=1)
+
+        correct += (
+            predicted == batch["label"]
+        ).sum().item()
+
+        total += len(predicted)
+
+accuracy = correct / total
+
+print(f"Accuracy : {accuracy:.2%}")
+
+
+
+# ========================================================
+# ========================================================
+# 💾 Save
+torch.save(
+    model.state_dict(),
+    "smart_intent_ai.pth"
+)
+
+print("✅ Model Saved")
