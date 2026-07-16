af1c94e4cf259afae7a83ddf19d0983d0a9b58b4
StarDust130 <csyadav0513@gmail.com>
2026-07-16 11:50:49 +0530

    CharacterTokenizer done

---
+++ projects/TinyTransformer/tokenizer.py
@@ -0,0 +1,33 @@
+
+class CharacterTokenizer:
+
+    def __init__(self, text):
+
+        # Find every unique character
+        chars = sorted(set(text))
+
+        # Character → Number
+        self.stoi = {
+            ch: i
+            for i, ch in enumerate(chars)
+        }
+
+        # Number → Character
+        self.itos = {
+            i: ch
+            for ch, i in self.stoi.items()
+        }
+
+    def encode(self, text):
+
+        return [
+            self.stoi[ch]
+            for ch in text
+        ]
+
+    def decode(self, ids):
+
+        return "".join(
+            self.itos[i]
+            for i in ids
+        )
