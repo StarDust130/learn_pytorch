42bf784d7a54fed21cd61623c1bf443091133497
StarDust130 <csyadav0513@gmail.com>
2026-07-16 13:53:04 +0530

    fuck chat gpt

--- projects/TinyTransformer/embedding.py
+++ projects/TinyTransformer/embedding.py
@@ -11,3 +11,13 @@ token_ids = torch.tensor([2, 5, 7])
 vectors = embedding(token_ids)
 
 print(vectors)
+
+
+position_embedding = nn.Embedding(
+    num_embeddings=128,
+    embedding_dim=32
+)
+
+positions = torch.arange(5)
+
+position_vectors = position_embedding(positions)
